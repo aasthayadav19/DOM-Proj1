6642d5d5c2b6eba51bed6c989453d9adf1f2fb10
aasthayadav19 <aasthayadav052000@gmail.com git config --global user.name>
2026-02-06 14:48:49 +0530

    Updated DOM project

---
+++ script.js
@@ -0,0 +1,23 @@
+let istatus = document.querySelector("h5")
+
+let addFriend = document.querySelector("#add")
+
+var check = 0
+
+addFriend.addEventListener("click",function(){
+if(check == 0){
+    istatus.innerHTML = ("Friends")
+    istatus.style.color = "green"
+    addFriend.innerHTML = "Remove Friend"
+   
+    check = 1
+
+}
+else{
+        istatus.innerHTML = ("Strangers")
+        istatus.style.color = "red"
+        addFriend.innerHTML = ("Add Friend")
+
+        check = 0
+}
+})
